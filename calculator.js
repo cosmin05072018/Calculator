a87d4b41d43c0bc5bbb539a4d710786b2d8728bb
Morari Cosmin <cosminmorari99@yahoo.com>
2022-02-24 16:39:51 -0800

    js add

--- calculator.js
+++ calculator.js
@@ -2,18 +2,33 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
     clear() {
-
+        this.currentOperand=''
+        this.previousOperand=''
+        this.operation = undefined
     }
 
     delete(){
 
     }
 
-    appendNumber(){
-        
+    appendNumber(number){
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
+
+    chooseOperation(operation){
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay(){
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 
@@ -30,3 +45,12 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]'); 
 const currentOperandTextElement = document.querySelector('[data-current-operand]'); 
 
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
